e5aea57f811f2a7b53d993cf905f82ac0275e00e
whshimeji <spule634@gmail.com>
2018-07-09 23:11:23 +0900

    first commit

---
+++ local_run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export HUBOT_TWITTER_KEY="ZzsOxQoxWcFLcbN4gz3ITVTVs"
+export HUBOT_TWITTER_SECRET="qUo59sIIbugcLlErxHJM84RkLvpOkTVziARbL5Yu7i7O5V1wFA"
+export HUBOT_TWITTER_TOKEN="1012339866602897408-eFXkAySVamSGQrquEU3hmxEkzGNOWa"
+export HUBOT_TWITTER_TOKEN_SECRET="QIexyyxuGjuVuao3Yfdr5ZCihPQ65TZhzT2dxu3339FBW"
+
+bin/hubot -a twitter -n @whweiss_bot
+bin/hubot -a twitter -n @whweiss_bot
